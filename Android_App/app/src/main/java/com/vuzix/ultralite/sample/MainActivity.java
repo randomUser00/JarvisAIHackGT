c2c6ae5c7f3c6a3f934df3086dca0be60c2d308a
anvith <anand.anvith@gmail.com>
2024-09-28 22:14:10 -0400

    add drawable

--- Android_App/app/src/main/java/com/vuzix/ultralite/sample/MainActivity.java
+++ Android_App/app/src/main/java/com/vuzix/ultralite/sample/MainActivity.java
@@ -14,6 +14,7 @@ import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.net.Uri;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
